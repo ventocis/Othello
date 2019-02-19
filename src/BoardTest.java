b4d173942ae143afdf4cff044ac793baca4d5a33
Chandler Scott <scottcha@mail.gvsu.edu>
2019-02-19 10:41:03 -0500

    Create BoardTest.java
    Basic JUnit set up for board

---
+++ src/BoardTest.java
@@ -0,0 +1,72 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class BoardTest {
+
+	@Test
+	void testBoard() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetPlyr() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetPlyr() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testNextPlyr() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetCurrPlyr() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testIsValidMove() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCheckDirections() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testPlayPiece() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetWinner() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testNewGame() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testWhiteScore() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testBlackScore() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCanMove() {
+		fail("Not yet implemented");
+	}
+
+}
