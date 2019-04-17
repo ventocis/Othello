600e87d6800f688de7366b1deb084eb03cc372af
alenhurke <35513103+alenhurke@users.noreply.github.com>
2019-04-17 00:28:16 -0400

    Update Piece.java

--- src/Piece.java
+++ src/Piece.java
@@ -1,13 +1,13 @@
 import java.awt.*;
 
-public class CheckersPiece {
+public class Piece {
 	
 	private int row;
 	private int col;
 	public Color color;
 	
 	
-	public CheckersPiece(Color color, int row, int col) {
+	public Piece(Color color, int row, int col) {
 		this.color = color;
 		this.row = row;
 		this.col = col;
