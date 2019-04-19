5d6bc22482831211223b83b26a00bd7c8e99a88e
Chandler Scott <43549158+cScottDeveloper@users.noreply.github.com>
2019-04-19 10:06:02 -0400

    Update CheckersPiece.java

--- src/CheckersPiece.java
+++ src/CheckersPiece.java
@@ -1,5 +1,11 @@
 import java.awt.*;
-
+/**
+ * @author Chandler Scott, Sam Ventocilla, Allen Huric
+ * This is the class that contains individual piece logic checkers
+ * board. 
+ * 
+ * @version 1.0
+ */
 public class CheckersPiece {
 	
 	private int row;
@@ -7,31 +13,52 @@ public class CheckersPiece {
 	public Color color;
 	
 	
-	 /** Created by Alen, Sam, Chandler */
+	/**
+	 * Constructor for the Checkers piece class.
+	 * @param color variable for the color of the piece.
+	 * @param row variable for the row on the board.
+	 * @param col variable for the column on the board.
+	 */
 	public CheckersPiece(Color color, int row, int col) {
 		this.color = color;
 		this.row = row;
 		this.col = col;
 	}
-	
+	/**
+	 * Method to get the row of the board.
+	 * @return gets the variable row.
+	 */
 	public int getRow() {
 		return row;
 	}
-	
+	/**
+	 * Method to get the color of the piece on the board.
+	 * @return gets the variable color for the piece.
+	 */
 	public Color getColor() {
 		return color;
 	}
-	
+	/**
+	 * Method to get the column of the board.
+	 * @return the column of the board.
+	 */
 	public int getCol() {
 		return col;
 	}
-	
+	/**
+	 * Method to set the row and column for each piece.
+	 * @param row variable for the row on board.
+	 * @param col variable for the column on board.
+	 */
 	public void setRowCol(int row, int col) {
 		this.row = row;
 		this.col = col;
 	}
 
-	 /** Created by Alen, Sam, Chandler */
+	/**
+	 * Simple to string method that converts various values to string
+	 * in order to display status.
+	 */
 	public String toString() {
 		
 		StringBuilder s = new StringBuilder();
