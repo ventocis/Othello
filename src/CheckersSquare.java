b915c9778d5c46a35f2fecff275afb100de45698
Chandler Scott <43549158+cScottDeveloper@users.noreply.github.com>
2019-04-19 10:17:06 -0400

    Update CheckersSquare.java

--- src/CheckersSquare.java
+++ src/CheckersSquare.java
@@ -1,5 +1,11 @@
 import java.awt.*;
-
+/**
+ * @author Chandler Scott, Sam Ventocilla, Allen Huric
+ * This is the class that contains individual square logic checkers
+ * board. 
+ * 
+ * @version 1.0
+ */
 @SuppressWarnings("serial")
 public class CheckersSquare extends Canvas {
     public enum BackgroundColor { LIGHT, DARK };
@@ -10,7 +16,12 @@ public class CheckersSquare extends Canvas {
     private int row;
     private int col;
 	
-	 /** Created by Alen, Sam, Chandler */
+	/**
+     * Constructor for the square class.
+     * @param c variable for the background color of each square.
+     * @param row variable representing the row for each square.
+     * @param col variable representing the column for each square.
+     */
     public CheckersSquare(BackgroundColor c, int row, int col) {
     	
     	this.setSize(64, 64);
@@ -28,32 +39,49 @@ public class CheckersSquare extends Canvas {
         this.col = col;
         
     }
-
+	    /**
+     * Method to check if square contains piece.
+     * @return returns true if has a piece. 
+     */
     public boolean hasPiece() {
         return this.isPieceExisting;
     }
-    
+     /**
+     * Method to return row of square.
+     * @return returns the row location.
+     */
     public int getRow() {
     	return this.row;
     }
-    
+     /**
+     * Method to return the column of square.
+     * @return returns the column location.
+     */
     public int getCol() {
     	return this.col;
     }
-    
+     /**
+     * Method to return the background color of the square.
+     * @return returns the current color.
+     */
     public CheckersSquare.BackgroundColor getBackgroundColor() {
     	return this.bgColor;
     }
     
-	 /** Created by Alen, Sam, Chandler */
+	    /**
+     * Method to get the piece on the square.
+     * @return returns the piece.
+     */
     public CheckersPiece getPiece() {
     	if(this.hasPiece())
     		return this.CheckersPiece;
     	
     	return null;
     }
-
-	 /** Created by Alen, Sam, Chandler */
+	
+	/**
+     	* Method to change the color of the square to its proper color.
+     	*/
 	public void paint(Graphics g) {
         if (this.getBackgroundColor() == CheckersSquare.BackgroundColor.DARK) {
 			this.setBackground(Color.DARK_GRAY);
@@ -69,7 +97,10 @@ public class CheckersSquare extends Canvas {
         }			
 	}    
 	 
-	 /** Created by Alen, Sam, Chandler */
+	/**
+	 * Sets the piece to its valid location. 
+	 * @param visitor acts as placeholder if piece is there.
+	 */
 	public void setPiece(CheckersPiece visitor) {
     	if(visitor != null) {
     		this.CheckersPiece = visitor;
@@ -81,8 +112,11 @@ public class CheckersSquare extends Canvas {
     		this.isPieceExisting = false;
     	}
     }
-
-	 /** Created by Alen, Sam, Chandler */
+	
+		/**
+	 * Sets the highlight color of the square when used.
+	 * @param doHighlight flag for performing the highlight.
+	 */
     public void setHighlight(boolean doHighlight) {
     	
     	
